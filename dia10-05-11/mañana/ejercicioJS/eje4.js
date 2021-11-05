4ab374ee23f0da4584098186f25fb256a4443386
Facundo <facuunion13@gmail.com>
2021-11-05 10:18:35 -0300

    asignar el valore retorno

--- dia10-05-11/mañana/ejercicioJS/eje4.js
+++ dia10-05-11/mañana/ejercicioJS/eje4.js
@@ -5,8 +5,7 @@ function arrayInvertido(array){
     for(var i=array.length-1; i>=0; i--){
         aux.push(array[i]);
     }
-    array=aux;
-    return array;
+    return aux;
 }
 
 let array =[1,2,3,4];
