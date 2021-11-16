559fd7e7dc60855ad1bc8e5cace8a98cf27cb673
Facundo <facuunion13@gmail.com>
2021-11-16 12:25:30 -0300

    agregar formado y acitvo al toggle

--- dia16-16-11/mañana/actividad2/main.js
+++ dia16-16-11/mañana/actividad2/main.js
@@ -16,82 +16,81 @@ Crear un form completamente desde javascript
 para insertar en nuestro html */
 
 //Elementos que existen
-const btn= document.querySelector(".btnVisibilidad");
-const span= document.querySelector(".span");
-const container= document.querySelector(".container")
+const btn = document.querySelector(".btnVisibilidad");
+const span = document.querySelector(".span");
+const container = document.querySelector(".container")
 
-function cambiarVisibilidad(){
-    //p.toggle("slow");
-   const result= span.classList.toggle("c");
-   if(result){
-       span.textContent="Elemento"
-   }
-   else{
-       span.textContent=""
-   }
+function cambiarVisibilidad() {
+    const result = span.classList.toggle("activo");
+    if (result) {
+        span.textContent = "Elemento"
+    }
+    else {
+        span.textContent = ""
+    }
 }
 btn.addEventListener("click", cambiarVisibilidad);
 
 //Elementos Creados
 
-const ul=document.createElement("ul");
+const ul = document.createElement("ul");
 container.appendChild(ul);
-const input=document.createElement("input");
+const input = document.createElement("input");
 container.appendChild(input);
-const btonAdd=document.createElement("button");
-btonAdd.textContent ="Agregar li";
+const btonAdd = document.createElement("button");
+btonAdd.textContent = "Agregar li";
 container.appendChild(btonAdd);
 
-function agregarLi(){
-    if(input.value.trim().length!=0){
-        let li=document.createElement("li");
-        li.textContent=input.value;
+function agregarLi() {
+    if (input.value.trim().length != 0) {
+        let li = document.createElement("li");
+        li.textContent = input.value;
         ul.appendChild(li);
     }
 }
 btonAdd.addEventListener("click", agregarLi);
 
-let año=new Date();
-const añoHtml=document.createElement("p");
-añoHtml.textContent="Hola, el año actual es: "+año.getFullYear();
+let año = new Date();
+const añoHtml = document.createElement("p");
+añoHtml.textContent = "Hola, el año actual es: " + año.getFullYear();
 container.appendChild(añoHtml);
 
-let contenedor=document.createElement("div");
+let contenedor = document.createElement("div");
 container.appendChild(contenedor);
-contenedor.style.width="400px";
-contenedor.style.height="400px";
-contenedor.style.border="1px solid black";// agrege para que sea visible
-contenedor.addEventListener("click",()=>contenedor.style.background="red");
-let btn1=document.createElement("button");
-btn1.textContent="Boton1";
+contenedor.style.width = "400px";
+contenedor.style.height = "400px";
+contenedor.style.border = "1px solid black";// agrege para que sea visible
+contenedor.addEventListener("click", () => contenedor.style.background = "red");
+let btn1 = document.createElement("button");
+btn1.textContent = "Boton1";
 contenedor.appendChild(btn1);
-let btn2=document.createElement("button");
-btn2.textContent="Boton2";
+let btn2 = document.createElement("button");
+btn2.textContent = "Boton2";
 contenedor.appendChild(btn2);
 
-btn1.addEventListener("click",()=>console.log("boton1 se ejectuo"));
-btn2.addEventListener("click",()=>console.log("boton2 se ejectuo"));
+btn1.addEventListener("click", () => console.log("boton1 se ejectuo"));
+btn2.addEventListener("click", () => console.log("boton2 se ejectuo"));
 
-const form=document.createElement("form");
+const form = document.createElement("form");
 container.appendChild(form);
-const labelNombre=document.createElement("label");
+const labelNombre = document.createElement("label");
 form.appendChild(labelNombre);
-labelNombre.textContent="Nombre:";
-const inputNombre=document.createElement("input");
+labelNombre.textContent = "Nombre:";
+const inputNombre = document.createElement("input");
 form.appendChild(inputNombre);
-const labelApellido=document.createElement("label");
+const labelApellido = document.createElement("label");
 form.appendChild(labelApellido);
-labelApellido.textContent="Apellido:";
-const inputApellido=document.createElement("input");
+labelApellido.textContent = "Apellido:";
+const inputApellido = document.createElement("input");
 form.appendChild(inputApellido);
 form.style.width = "250px"
 //form.style.display = "flex";
 //form.style.flex.direction = "column";
-const labelMail=document.createElement("label");
+const labelMail = document.createElement("label");
 form.appendChild(labelMail);
-labelMail.textContent="Mail:";
-const inputMail=document.createElement("input");
+labelMail.textContent = "Mail:";
+const inputMail = document.createElement("input");
 form.appendChild(inputMail);
-const btnEnviar=document.createElement("button");
-btnEnviar.textContent="Enviar";
+const btnEnviar = document.createElement("button");
+btnEnviar.textContent = "Enviar";
 form.appendChild(btnEnviar);
