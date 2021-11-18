44afeeb1f46f93a44eaacea7890bce36d6d00e26
Facundo <facuunion13@gmail.com>
2021-11-18 15:17:33 -0300

    js mejorado

--- dia18-18-11/mañana/sidebar/main.js
+++ dia18-18-11/mañana/sidebar/main.js
@@ -1,48 +1,32 @@
-const siguiente=document.querySelector(".siguiente");
-const atras=document.querySelector(".atras");
-const img1=document.querySelector(".img1");
-const img2=document.querySelector(".img2");
-const img3=document.querySelector(".img3");
-const cantImag=3;
-let i=0;
+/* DOM */
+const siguiente = document.querySelector(".siguiente");
+const atras = document.querySelector(".atras");
+const arrImg = document.querySelectorAll(".img");
 
-siguiente.addEventListener('click',()=>{
-    i++;;
-    switch(Math.abs(i% cantImag)){
-        case 0:
-            img1.classList.add("activo");
-            img2.classList.remove("activo")
-            img3.classList.remove("activo")
-            break;
-        case 1:
-            img1.classList.remove("activo")
-            img2.classList.add("activo");
-            img3.classList.remove("activo")
-            break;
-        case 2:
-            img1.classList.remove("activo")
-            img2.classList.remove("activo")
-            img3.classList.add("activo");
-            break;
-    }
-});
-atras.addEventListener('click',()=>{
+/* variables */
+const cantImag = 3;
+let i = 0;
+
+function actualizarFoto(){
+    
+    let resto = Math.abs(i % 3);
+    arrImg.forEach((el, i) => {
+        if (i == resto) {
+            el.classList.add("activo");
+        }
+        else {
+            el.classList.remove("activo")
+        }
+    })
+}
+
+siguiente.addEventListener('click', () => {
+    i++;
+    actualizarFoto();
+}); 
+
+
+atras.addEventListener('click', () => {
     i--;
-    switch(Math.abs(i% cantImag)){
-        case 0:
-            img1.classList.add("activo");
-            img2.classList.remove("activo")
-            img3.classList.remove("activo")
-            break;
-        case 1:
-            img1.classList.remove("activo")
-            img2.classList.add("activo");
-            img3.classList.remove("activo")
-            break;
-        case 2:
-            img1.classList.remove("activo")
-            img2.classList.remove("activo")
-            img3.classList.add("activo");
-            break;
-    }
-});
+    actualizarFoto();
+}); 
